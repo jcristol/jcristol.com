d82c2be47253cfd71d52a0bac660b4ed9178e48b
jcristol <joshcristol@gmail.com>
2017-12-25 13:59:35 -0600

    cleaned up the index as well

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import WebFont from 'webfontloader';
-
-WebFont.load({
-  google: {
-    families: ['Titillium Web:300,400,700', 'sans-serif']
-  }
-});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

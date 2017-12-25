494ae98823f4a1b2fa2cc4dfc0a614b8b0d2180a
jcristol <joshcristol@gmail.com>
2017-12-25 13:38:46 -0600

    added and changed the website font

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import WebFont from 'webfontloader';
 
 WebFont.load({
   google: {
-    families: ['Titillium Web:300,400,700', 'sans-serif']
+    families: ['Work Sans:300,400,700', 'sans-serif']
   }
 });
 

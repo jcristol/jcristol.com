25bc547ebd4e1aecc932ef1db3c608c1acb672a8
jcristol <joshcristol@gmail.com>
2018-01-29 12:32:57 -0600

    using the browser router syntax site still working

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>), document.getElementById('root'));
 registerServiceWorker();

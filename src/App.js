151addf58450df6aca9cda66ecc2d9c30257c11d
jcristol <joshcristol@gmail.com>
2018-01-29 14:08:49 -0600

    got both blog post on the website

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Header from './Header';
 import Meat from './Meat';
 import Footer from './Footer';
 import Blog1 from './blog/JS/Blog1';
+import Blog2 from './blog/JS/Blog2';
 import { Route, Switch } from 'react-router';
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route path='/blog1' component={Blog1}/>
+        <Route path='/Blog1' component={Blog1}/>
+        <Route path='/Blog2' component={Blog2}/>
         <Route path='/'component={PersonalSite}/>
       </Switch>
     );

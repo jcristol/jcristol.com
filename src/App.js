45b60fb2ec0c30a84e75a5405c4973eaf9c7d24d
jcristol <joshcristol@gmail.com>
2018-01-29 12:46:37 -0600

    blog functionailty is here have not tested markdown yet

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import './css/style.css';
 import Header from './Header';
 import Meat from './Meat';
 import Footer from './Footer';
+import Blog1 from './blog/JS/Blog1';
+import { Route, Switch } from 'react-router';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Meat />
-        <Footer />
-      </div>
+      <Switch>
+        <Route path='/blog1'>{Blog1}</Route>
+        <Route path='/'>{PersonalSite}</Route>
+      </Switch>
     );
   }
 }
+
+const PersonalSite = () => (
+  <div>
+    <Header />
+    <Meat />
+    <Footer />
+  </div>
+); 
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
 
